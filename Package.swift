18a34fa7364befa6de0d50fb4f70be1d5560d2bd
Özgün Ergen <>
2021-05-01 04:21:17 +0300

    bug fix Swift PM version

--- Package.swift
+++ Package.swift
@@ -1,16 +1,25 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Networking",
-    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
     products: [
-        .library(name: "Networking", targets: ["Networking"])
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Networking",
+            targets: ["Networking"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Networking",
-            path: "Networking"
-        )
+            dependencies: []),
+        
     ]
 )
