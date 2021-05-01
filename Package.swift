207d7bfd0ca17b80884158708882bbf0c07a25a5
Özgün Ergen <>
2021-05-01 04:25:54 +0300

    bug fix

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Networking",
-            dependencies: []),
+            path:"Networking")
         
     ]
 )
