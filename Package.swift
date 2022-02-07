1d6ef688107d7c98219003103fc27c6fe3b7c9fc
Özgün Ergen <orhanozgun.ergen@kocsistem.com.tr>
2022-02-07 14:59:54 +0300

    bug fix

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "Networking", dependencies: ["loading"], path: "Networking"),
+        .target(name: "Networking", dependencies: ["loading",.target(name: "LoadingLibrary"),], path: "Networking"),
             
         
     ]
