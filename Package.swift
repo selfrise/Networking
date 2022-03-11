bcc1f7a424252b8f8cf4c5a11b23fe54ff00b389
Tolga YILDIRIM <tolgaayildirimm@outlook.com>
2022-03-11 11:50:12 +0300

    unnecessary dependencies removed

--- Package.swift
+++ Package.swift
@@ -9,16 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Networking",
-            targets: ["Networking"]),
+            targets: ["Networking"])
     ],
-    dependencies: [
-            .package(url: "https://gitlab.kocsistem.com.tr/oneframe-mobile/ios/loading", from: "2.0.0")
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "Networking", dependencies: [.product(name: "LoadingLibrary", package: "loading") ], path: "Networking"),
-            
-        
+        .target(
+            name: "Networking",
+            path: "Networking"
+        )
     ]
 )
